590d1ba12b09d6031e392da77a3c76fc7db219de
Zoplos <giannis97z97z@gmail.com>
2022-05-06 16:58:30 +0300

    fixed ex 3.15-3.18

--- index.js
+++ index.js
@@ -53,11 +53,13 @@ app.get("/", (req, res) => {
   res.send("<h1>Hello World!</h1>");
 });
 
-app.get("/api/persons", (req, res) => {
+app.get("/api/persons", (req, res, next) => {
   // res.json(persons);
-  Person.find({}).then((persons) => {
-    res.json(persons);
-  });
+  Person.find({})
+    .then((persons) => {
+      res.json(persons);
+    })
+    .catch((error) => next(error));
 });
 
 app.get("/api/persons/:id", (req, res, next) => {
@@ -78,13 +80,15 @@ app.delete("/api/persons/:id", (req, res, next) => {
     .catch((error) => next(error));
 });
 
-app.get("/info", (req, res) => {
+app.get("/info", (req, res, next) => {
   const date = new Date();
-  Person.find({}).then((persons) => {
-    res.send(
-      `<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`
-    );
-  });
+  Person.find({})
+    .then((persons) => {
+      res.send(
+        `<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`
+      );
+    })
+    .catch((error) => next(error));
 });
 
 app.post("/api/persons", (req, res) => {
